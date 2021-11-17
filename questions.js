f3424fce6fc5dc5caeec24ba7bebfd69170d58ff
RajeshwariSomasundaram <83411391+RajeshwariSomasundaram@users.noreply.github.com>
2021-11-17 21:03:02 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Indian to cross seven important seas by swimming",
+    answer: "Bula Chaudhury",
+    options: [
+      "Raja Deendayal",
+      "Rahul Dravid",
+      "Bula Chaudhury",
+      "Lawrence"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Crystal Wedding Anniversary is known for how many years?",
+    answer: "15",
+    options: [
+      "12",
+      "10",
+      "20",
+      "15"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does the country Canada mean in english?",
+    answer: "Big Village",
+    options: [
+      "Waterfall City",
+      "Land of Maple tree",
+      "Big Village",
+      "Maple Leaf Village"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Markup Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What is 'RPG'- a business oriented programming language?",
+    answer: "Report Program Generator",
+    options: [
+      "Report Program Generator",
+      "Revised Program Generator",
+      "Report Plan Generator",
+      "Revised Plan Generator"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What is one yard equivalent to?",
+    answer: "All of these",
+    options: [
+      "90 cm",
+      "3 feet",
+      "36 inches",
+      "All of these"
+    ]
+  },
+    {
+    numb: 8,
+    question: "What does the abbreviation 'PDA' stands for?",
+    answer: "Personal Digital Assistant",
+    options: [
+      "Personal Data Analyst",
+      "Personal Digital Assistant",
+      "Professional Digital Analyst",
+      "Profession Data Analyzer"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Which year among these is not a leap year?",
+    answer: "2050",
+    options: [
+      "1200",
+      "1600",
+      "2000",
+      "2050"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Which number among these cannot be the multiples of 8?",
+    answer: "180",
+    options: [
+      "80",
+      "180",
+      "280",
+      "480"
+    ]
+  },
+];
